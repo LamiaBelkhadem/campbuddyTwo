774f9c1a5cd24eebb385736d2a6127c4b7e1cffb
yassinebk <yassinebk23@gmail.com>
2023-12-16 20:44:24 +0100

    refactor(front): fix bugs

---
+++ src/components/reviews/RealtimeReviewsList.jsx
@@ -0,0 +1,76 @@
+import React, {useState, useEffect} from 'react';
+import TextField from '@mui/material/TextField';
+import Button from '@mui/material/Button';
+import io from 'socket.io-client';
+
+const socket = io(process.env.VITE_WS_URL); // Adjust the URL to match your server
+
+const RealTimeReviewComponent = () => {
+    const [reviews, setReviews] = useState([]);
+    const [newReviewText, setNewReviewText] = useState('');
+    const [connectedUsers, setConnectedUsers] = useState([]);
+
+    useEffect(() => {
+        // Listen for new reviews from the server
+        socket.on('newReview', (newReview) => {
+            setReviews((prevReviews) => [...prevReviews, newReview]);
+        });
+
+        // Listen for userConnected events
+        socket.on('userConnected', (data) => {
+            setConnectedUsers((prevUsers) => [...prevUsers, data.userId]);
+        });
+
+        // Listen for userDisconnected events
+        socket.on('userDisconnected', (data) => {
+            setConnectedUsers((prevUsers) => prevUsers.filter((userId) => userId !== data.userId));
+        });
+
+        // Clean up the socket connection when the component unmounts
+        return () => {
+            socket.disconnect();
+        };
+    }, []);
+
+    const handleNewReviewSubmit = () => {
+        // Emit a new review to the server
+        socket.emit('newReview', {text: newReviewText});
+
+        // Clear the input field
+        setNewReviewText('');
+    };
+
+    return (
+        <div>
+            <div>
+                <TextField
+                    label="New Review"
+                    variant="outlined"
+                    value={newReviewText}
+                    onChange={(e) => setNewReviewText(e.target.value)}
+                />
+                <Button variant="contained" color="primary" onClick={handleNewReviewSubmit}>
+                    Submit Review
+                </Button>
+            </div>
+            <div>
+                <h2>Reviews</h2>
+                <ul>
+                    {reviews.map((review, index) => (
+                        <li key={index}>{review.text}</li>
+                    ))}
+                </ul>
+            </div>
+            <div>
+                <h2>Connected Users</h2>
+                <ul>
+                    {connectedUsers.map((userId) => (
+                        <li key={userId}>{`User ${userId} connected`}</li>
+                    ))}
+                </ul>
+            </div>
+        </div>
+    );
+};
+
+export default RealTimeReviewComponent;
--- src/pages/Profile/Profile.jsx
+++ src/pages/Profile/Profile.jsx
@@ -6,67 +6,67 @@ import ProfileDetails from "../../components/profileDetails/ProfileDetails.jsx";
 import SidebarProfil from "../../components/SidebarProfile/SidebarProfil.jsx";
 import Socials from "../../components/socials/Socials.jsx";
 import "./profile.css";
-import {useAuth} from "../../hooks/useAuth.jsx";
-import {useGetMyProfile} from "../../hooks/api/profile/useGetMyProfile.jsx";
+import { useAuth } from "../../hooks/useAuth.jsx";
+import { useGetMyProfile } from "../../hooks/api/profile/useGetMyProfile.jsx";
 import LoadingPage from "../../components/common/loading/LoadingPage.jsx";
 
 export default function Profile() {
-    const {data: profile, isLoading} = useGetMyProfile()
-    const {user} = useAuth()
+  const { data: profile, isLoading } = useGetMyProfile();
+  const { user } = useAuth();
 
-    if (isLoading) return <LoadingPage/>
+  if (isLoading) return <LoadingPage />;
 
-    return (
-        <div className="profile">
-            <Navbar/>
-            <div className="profile1-container">
-                <div className="left-side">
-                    {profile && (
-                        <SidebarProfil
-                            className="sidebar"
-                            username={profile.username}
-                            lobbies={profile.lobbies}
-                        />
-                    )}
-                </div>
-
-                <div className="center">
-                    <div className="center-bottom">
-                        {profile && (
-                            <ProfileDetails
-                                fname={profile.fname}
-                                lname={profile.lname}
-                                area={profile.area}
-                                age={profile.age}
-                                email={user.email}
-                            />
-                        )}
-                        {profile && (
-                            <Socials
-                                facebook={profile.facebook}
-                                instagram={profile.instagram}
-                                twitter={profile.twitter}
-                                tiktok={profile.tiktok}
-                            />
-                        )}
-                    </div>
-
-                    <div className="center-bottom">
-                        {profile && (
-                            <CamperInfo
-                                gender={profile.gender}
-                                experience={profile.experience}
-                                aboutme={profile.desc}
-                                interests={profile.interests}
-                                equipment={profile.equipment}
-                                favourites={profile.favourites}
-                            />
-                        )}
-                        <AccountDetails/>
-                    </div>
-                </div>
-            </div>
-            <Footer/>
+  return (
+    <div className="profile">
+      <Navbar />
+      <div className="profile1-container">
+        <div className="left-side">
+          {profile && (
+            <SidebarProfil
+              className="sidebar"
+              username={profile.username}
+              lobbies={profile.lobbies}
+            />
+          )}
         </div>
-    );
+
+        <div className="center">
+          <div className="center-bottom">
+            {profile && (
+              <ProfileDetails
+                fname={profile.fname}
+                lname={profile.lname}
+                area={profile.area}
+                age={profile.age}
+                email={user.email}
+              />
+            )}
+            {profile && (
+              <Socials
+                facebook={profile.facebook}
+                instagram={profile.instagram}
+                twitter={profile.twitter}
+                tiktok={profile.tiktok}
+              />
+            )}
+          </div>
+
+          <div className="center-bottom">
+            {profile && (
+              <CamperInfo
+                gender={profile.gender}
+                experience={profile.experience}
+                aboutme={profile.desc}
+                interests={profile.interests}
+                equipment={profile.equipment}
+                favourites={profile.favourites}
+              />
+            )}
+            <AccountDetails />
+          </div>
+        </div>
+      </div>
+      <Footer />
+    </div>
+  );
 }
