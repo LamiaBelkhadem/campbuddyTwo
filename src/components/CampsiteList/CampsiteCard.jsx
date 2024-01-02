b8af81e09de125256af8f8c124f06d6ff8735e8b
Your NameLamia <swe2009867@xmu.edu.my>
2024-01-03 05:49:43 +0800

    commit

--- src/components/CampsiteList/CampsiteCard.jsx
+++ src/components/CampsiteList/CampsiteCard.jsx
@@ -1,13 +1,18 @@
 import { Link } from "react-router-dom";
-
+import { getImageURL } from "../../../utils/getImageURL.js";
 import CampsitePictureCarousel from "./CampsitePictureCarousel.jsx";
+import { useEffect, useState } from "react";
+
 import {
   Button,
   Card,
   CardContent,
   CardMedia,
   Typography,
+  Chip,
+  Box,
 } from "@mui/material";
+import InfoIcon from '@mui/icons-material/Info';
 
 const CampsiteCard = ({ campsite }) => {
   const {
@@ -22,39 +27,71 @@ const CampsiteCard = ({ campsite }) => {
     amenities,
   } = campsite;
 
+
+
+
+const [currentSlide, setCurrentSlide] = useState(0);
+
+const nextSlide = () => {
+  setCurrentSlide((prev) => (prev + 1) % images.length);
+};
+
+const prevSlide = () => {
+  setCurrentSlide((prev) => (prev - 1 + images.length) % images.length);
+};
   return (
-    <Card>
-      <CardMedia component={"img"} height={200} image={mainImg} alt={name} />
-      <CampsitePictureCarousel imgs={images} />
-      <CardContent>
-        <Typography variant="h5" component="div">
-          {name}
-        </Typography>
-        <Typography variant="subtitle1" color="text.secondary">
-          {location}
-        </Typography>
-        <Typography variant="body2" color="text.secondary">
-          {desc}
-        </Typography>
-        <Typography variant="body2" color="text.secondary">
-          Category: {category}
-        </Typography>
-        <Typography variant="body2" color="text.secondary">
-          Security: {security ? "Yes" : "No"}
-        </Typography>
-        <Typography variant="body2" color="text.secondary">
-          Amenities: {amenities}
-        </Typography>
-        <Button
-          component={Link}
-          to={`/campsite/${_id}`}
-          variant="contained"
-          color="primary"
-        >
-          View Details
-        </Button>
-      </CardContent>
-    </Card>
+    <Box m={2}>
+      <Card sx={{width:"1000px"}}>
+       
+ <div className="carousel">
+          <div className="carousel-slides">
+            {images.map((image, index) => (
+              <div
+                className={index === currentSlide ? "slide active" : "slide"}
+                key={index}
+              >
+                <img
+                  src={getImageURL(image)}
+                  alt=""
+                />
+              </div>
+            ))}
+          </div>
+          <button className="carousel-control prev" onClick={prevSlide}>
+            &#10094;
+          </button>
+          <button className="carousel-control next" onClick={nextSlide}>
+            &#10095;
+          </button>
+        </div>        <CardContent>
+          <Typography variant="h5" component="div" gutterBottom>
+            {name}
+          </Typography>
+          <Typography variant="h6" color="text.secondary">
+            {location}
+          </Typography>
+          <Typography variant="body1" color="text.secondary" style={{width:"60vw", marginTop:"20px", marginBottom:"20px", textAlign:"justify"}}>
+            {desc}
+          </Typography>
+          <div style={{display:"table-row"}}>
+          <Chip label={`Category: ${category}`} variant="outlined" style={{marginRight:"20px"}} />
+          <Chip label={`Security: ${security ? "Yes" : "No"}`} style={{marginRight:"20px"}} variant="outlined" />
+          <Chip label={`Amenities: ${amenities}`} variant="outlined"style={{marginRight:"120px"}} />
+
+          <Button
+            component={Link}
+            sx={ {marginRight:"60px !important"}}
+            to={`/campsite/${_id}`}
+            variant="contained"
+            color="primary"
+            startIcon={<InfoIcon sx={{color:"white !important", marginRight:"20px"}} />}
+          >
+            Edit
+          </Button>
+          </div>
+        </CardContent>
+      </Card>
+    </Box>
   );
 };
 
--- src/components/CampsiteList/CampsitePictureCarousel.jsx
+++ src/components/CampsiteList/CampsitePictureCarousel.jsx
@@ -1,28 +1,34 @@
 import React from "react";
-import { CarouselProvider, Slide, Slider } from "pure-react-carousel";
+import { CarouselProvider, Slide, Slider, ButtonBack, ButtonNext } from "pure-react-carousel";
 import { IconButton } from "@mui/material";
 import { NavigateBefore, NavigateNext } from "@mui/icons-material";
+import { getImageURL } from "../../../utils/getImageURL.js";
 
 const CampsitePictureCarousel = ({ imgs }) => {
+  console.log("images", imgs)
   return (
     <CarouselProvider
       naturalSlideWidth={100}
       naturalSlideHeight={125}
-      totalSlides={3}
+      totalSlides={imgs.length}
     >
       <Slider>
         {imgs.map((img, index) => (
           <Slide key={index} index={index}>
-            <img src={img} alt={`Campsite ${index + 1}`} />
+            <img src={getImageURL(img)} alt={`Campsite ${index + 1}`} style={{ width: '100%', objectFit: 'cover', maxHeight: '100px' }} />
           </Slide>
         ))}
       </Slider>
-      <IconButton>
-        <NavigateBefore />
-      </IconButton>
-      <IconButton>
-        <NavigateNext />
-      </IconButton>
+      <ButtonBack>
+        <IconButton>
+          <NavigateBefore />
+        </IconButton>
+      </ButtonBack>
+      <ButtonNext>
+        <IconButton>
+          <NavigateNext />
+        </IconButton>
+      </ButtonNext>
     </CarouselProvider>
   );
 };
