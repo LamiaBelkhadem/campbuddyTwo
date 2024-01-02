c512c08de0b4e8f9fb4ebd99fb8fd6268a19df50
Yassine Belkhadem <yassinebk23@gmail.com>
2024-01-02 23:45:06 +0100

    fix: navigate to admin after creation

--- src/hooks/api/campsites/useCreateCampsite.jsx
+++ src/hooks/api/campsites/useCreateCampsite.jsx
@@ -1,15 +1,18 @@
 import useAxios from "../../useAxios.jsx";
 import { createCampsite } from "../../../lib/api/campsites/index.js";
 import { useMutation, useQueryClient } from "@tanstack/react-query";
+import { useNavigate } from "react-router-dom";
 
 export const useCreateCampsite = () => {
   const { axios } = useAxios();
   const { invalidateQueries } = useQueryClient();
+  const navigate = useNavigate();
 
   return useMutation({
     mutationFn: (values) => createCampsite(values, axios),
     onSuccess: () => {
       invalidateQueries(["campsites"]);
+      navigate("/app/admin");
     },
   });
 };
